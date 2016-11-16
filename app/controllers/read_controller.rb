257b339c4a6050e664be52c65670e3c2cd2c3bdf
alexapperley <alexapperley@gmail.com>
2016-11-16 09:22:12 -0600

    added basic partial for items

--- app/controllers/read_controller.rb
+++ app/controllers/read_controller.rb
@@ -1,4 +1,9 @@
 class ReadController < ApplicationController
+  def front_page
+    @items = Item.all
+    @new_items = Item.order(:date_created)
+  end
   def index
+    @items = Item.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'read/index'
+  get '/', to: 'read#front_page', as: 'front_page'
+  get '/index', to: 'read#index', as: 'index'
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
