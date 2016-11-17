4c622fa977499759428b9c1b9005d2008752f47a
alexapperley <alexapperley@gmail.com>
2016-11-17 13:14:56 -0600

    added search, need to add search all option

--- app/controllers/read_controller.rb
+++ app/controllers/read_controller.rb
@@ -3,22 +3,31 @@ class ReadController < ApplicationController
     @staff_items = Item.joins(category_item: :category)\
       .where(categories: {name: 'Staff Picks'}).limit(5)
     @new_items = Item.order(created_at: :desc).limit(5)
+    get_categories
   end
   def index
-    @items = Item.where("name LIKE ?", "%#{params["search"]}%")
+    if(!params["category"].nil?) then
+      @items = Item.joins(category_item: :category)\
+        .where("categories.name IS ? AND items.name LIKE ?"\
+        ,*["#{params["category"]}","%#{params["search"]}%"])
+    else
+      @items = Item.where("name LIKE ?", "%#{params["search"]}%")
+    end
+    get_categories
   end
   def new_items
     @items = Item.order(created_at: :desc)
-  end
-  def category
-    @items = Item.joins(category_item: :category)\
-      .where("categories.name IS ? AND items.name LIKE ?"\
-      ,*["#{params["category"]}","%#{params["search"]}%"])
+    get_categories
   end
   def item
     @item = Item.where("name IS ?", "#{params["item"]}").first
+    get_categories
   end
   def other
     @page = Page.where("name IS ?", "#{params["page"]}").first
+    get_categories
+  end
+  private def get_categories
+    @categories = Category.all
   end
 end
